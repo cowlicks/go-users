3b4874ef2f7a1e6f22243bddef25aef7030dbb85
Blake Griffith <blake.a.griffith@gmail.com>
2016-05-10 21:23:40 -0500

    Run go fmt on code.
    This should be in a commit hook.

--- users.go
+++ users.go
@@ -6,121 +6,121 @@ Usernames must be unique.
 package users
 
 import (
-    "errors"
-    "database/sql"
-    _"github.com/mattn/go-sqlite3"
-    "golang.org/x/crypto/bcrypt"
+	"database/sql"
+	"errors"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
-    bcrypt_cost int = bcrypt.DefaultCost
+	bcrypt_cost int = bcrypt.DefaultCost
 )
 
 type UserCredentials struct {
-    username string
-    password string
+	username string
+	password string
 }
 
-func CreateUserTable(db * sql.DB) error {
-    sqlStmt := `
+func CreateUserTable(db *sql.DB) error {
+	sqlStmt := `
     CREATE TABLE IF NOT EXISTS userinfo(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
 		password TEXT
 	);
     `
-    _, err := db.Exec(sqlStmt)
-    return err
+	_, err := db.Exec(sqlStmt)
+	return err
 }
 
-func UserExists(db * sql.DB, username string) (bool, error) {
-    var un string
-    sqlStmt := `SELECT username FROM userinfo WHERE username = ?`
-    err := db.QueryRow(sqlStmt, username).Scan(&un)
+func UserExists(db *sql.DB, username string) (bool, error) {
+	var un string
+	sqlStmt := `SELECT username FROM userinfo WHERE username = ?`
+	err := db.QueryRow(sqlStmt, username).Scan(&un)
 
-    switch {
-    case err == sql.ErrNoRows: // no rows matched username, doesn't exist
-        return false, nil
-    case err != nil:           // a real error
-        return false, err
-    default:
-        return true, nil       // user exists
-    }
+	switch {
+	case err == sql.ErrNoRows: // no rows matched username, doesn't exist
+		return false, nil
+	case err != nil: // a real error
+		return false, err
+	default:
+		return true, nil // user exists
+	}
 }
 
-func CreateUser(db * sql.DB, uc UserCredentials) error {
-    // Check username isn't taken
-    exists, err := UserExists(db, uc.username)
-    if exists {
-        return errors.New("Username taken")
-    }
+func CreateUser(db *sql.DB, uc UserCredentials) error {
+	// Check username isn't taken
+	exists, err := UserExists(db, uc.username)
+	if exists {
+		return errors.New("Username taken")
+	}
 
-    // hash password
-    hashed_pw, err := bcrypt.GenerateFromPassword([]byte(uc.password),
-                                                  bcrypt_cost)
-    if err != nil {
-        return err
-    }
+	// hash password
+	hashed_pw, err := bcrypt.GenerateFromPassword([]byte(uc.password),
+		bcrypt_cost)
+	if err != nil {
+		return err
+	}
 
-    sqlStmt := `INSERT INTO userinfo(username, password) values(?,?)`
-    stmt, err := db.Prepare(sqlStmt)
-    if err != nil {
-        return err
-    }
+	sqlStmt := `INSERT INTO userinfo(username, password) values(?,?)`
+	stmt, err := db.Prepare(sqlStmt)
+	if err != nil {
+		return err
+	}
 
-    // User
-    _, err = stmt.Exec(uc.username, hashed_pw)
-    if err != nil {
-        return err
-    }
-    return nil
+	// User
+	_, err = stmt.Exec(uc.username, hashed_pw)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func VerifyCredentials(db * sql.DB, uc UserCredentials) bool {
-    var un string
-    var pw string
-    sqlStmt :=`SELECT username, password FROM userinfo WHERE username = ?`
+func VerifyCredentials(db *sql.DB, uc UserCredentials) bool {
+	var un string
+	var pw string
+	sqlStmt := `SELECT username, password FROM userinfo WHERE username = ?`
 
-    err := db.QueryRow(sqlStmt, uc.username).Scan(&un, &pw)
-    if err != nil {
-        return false
-    }
+	err := db.QueryRow(sqlStmt, uc.username).Scan(&un, &pw)
+	if err != nil {
+		return false
+	}
 
-    err2 := bcrypt.CompareHashAndPassword([]byte(pw), []byte(uc.password))
-    if err2 != nil {
-        return false
-    } else {
-        return true
-    }
+	err2 := bcrypt.CompareHashAndPassword([]byte(pw), []byte(uc.password))
+	if err2 != nil {
+		return false
+	} else {
+		return true
+	}
 }
 
-func UpdateUser(db * sql.DB, old_creds, new_creds UserCredentials) error {
-    updateStmt := `UPDATE userinfo SET username = ?, password = ? WHERE username = ?`
+func UpdateUser(db *sql.DB, old_creds, new_creds UserCredentials) error {
+	updateStmt := `UPDATE userinfo SET username = ?, password = ? WHERE username = ?`
 
-    verified := VerifyCredentials(db, old_creds)
-    if !verified {
-        return errors.New("The supplied credentials match no existing user.")
-    }
+	verified := VerifyCredentials(db, old_creds)
+	if !verified {
+		return errors.New("The supplied credentials match no existing user.")
+	}
 
-    hashed_pw, err2 := bcrypt.GenerateFromPassword([]byte(new_creds.password),
-                                                   bcrypt_cost)
-    if err2 != nil {
-        return err2
-    }
+	hashed_pw, err2 := bcrypt.GenerateFromPassword([]byte(new_creds.password),
+		bcrypt_cost)
+	if err2 != nil {
+		return err2
+	}
 
-    _, err3:= db.Exec(updateStmt, new_creds.username, hashed_pw, old_creds.username)
-    if err3 != nil {
-        return err3
-    }
-    return nil
+	_, err3 := db.Exec(updateStmt, new_creds.username, hashed_pw, old_creds.username)
+	if err3 != nil {
+		return err3
+	}
+	return nil
 }
 
-func DeleteUser(db * sql.DB, uc UserCredentials) error {
-    deleteStmt := `DELETE FROM userinfo WHERE username = ?`
-    verified := VerifyCredentials(db, uc)
-    if !verified {
-        return errors.New("The supplied credentials match no existing user.")
-    }
-    _, err := db.Exec(deleteStmt, uc.username)
-    return err
+func DeleteUser(db *sql.DB, uc UserCredentials) error {
+	deleteStmt := `DELETE FROM userinfo WHERE username = ?`
+	verified := VerifyCredentials(db, uc)
+	if !verified {
+		return errors.New("The supplied credentials match no existing user.")
+	}
+	_, err := db.Exec(deleteStmt, uc.username)
+	return err
 }
--- users_test.go
+++ users_test.go
@@ -1,92 +1,91 @@
 package users
 
 import (
-    "testing"
-    "os"
-    "database/sql"
+	"database/sql"
+	"os"
+	"testing"
 )
 
-func cleanDB(db * sql.DB) {
-    db.Close()
-    os.Remove("./foo.db")
+func cleanDB(db *sql.DB) {
+	db.Close()
+	os.Remove("./foo.db")
 }
 
 func check(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
+func TestDataBaseCRUD(t *testing.T) {
+	os.Remove("./foo.db")
+	db, err := sql.Open("sqlite3", "./foo.db")
+	check(err)
+	defer cleanDB(db)
 
-func TestDataBaseCRUD(t * testing.T) {
-    os.Remove("./foo.db")
-    db, err := sql.Open("sqlite3", "./foo.db")
-    check(err)
-    defer cleanDB(db)
+	creds := UserCredentials{username: "foo",
+		password: "This is such!_asecUR3 Passw0rd?"}
+	creds2 := UserCredentials{username: "alice or 1=1'*@;; FROM sqlimebro",
+		password: "baz"}
 
-    creds := UserCredentials{username: "foo",
-                             password: "This is such!_asecUR3 Passw0rd?"}
-    creds2 := UserCredentials{username: "alice or 1=1'*@;; FROM sqlimebro",
-                              password: "baz"}
+	CreateUserTable(db)
 
-    CreateUserTable(db)
+	// Create
+	err = CreateUser(db, creds)
+	check(err)
+	err = CreateUser(db, creds2)
+	check(err)
 
-    // Create
-    err = CreateUser(db, creds)
-    check(err)
-    err = CreateUser(db, creds2)
-    check(err)
+	exists, err := UserExists(db, creds.username)
+	check(err)
+	if exists == false {
+		t.Fail()
+	}
 
-    exists, err := UserExists(db, creds.username)
-    check(err)
-    if exists == false {
-       t.Fail()
-   }
+	no_exists, err := UserExists(db, "nonexistent user")
+	check(err)
+	if no_exists == true {
+		t.Fail()
+	}
 
-    no_exists, err := UserExists(db, "nonexistent user")
-    check(err)
-    if no_exists == true {
-        t.Fail()
-    }
+	// Read(Verify)
+	res := VerifyCredentials(db, creds)
+	if res != true {
+		t.Fail()
+	}
 
-    // Read(Verify)
-    res := VerifyCredentials(db, creds)
-    if res != true {
-        t.Fail()
-    }
+	res2 := VerifyCredentials(db, creds2)
+	if res2 != true {
+		t.Fail()
+	}
+	res3 := VerifyCredentials(db, UserCredentials{username: "yo",
+		password: "dog"})
+	if res3 == true {
+		t.Fail()
+	}
 
-    res2 := VerifyCredentials(db, creds2)
-    if res2 != true {
-        t.Fail()
-    }
-    res3 := VerifyCredentials(db, UserCredentials{username: "yo",
-                                                  password: "dog"})
-    if res3 == true {
-        t.Fail()
-    }
+	// Update
+	new_creds := UserCredentials{username: "Noam",
+		password: "Chomsky"}
+	err2 := UpdateUser(db, creds, new_creds)
+	check(err2)
+	res4 := VerifyCredentials(db, new_creds)
+	if res4 != true {
+		t.Fail()
+	}
 
-    // Update
-    new_creds := UserCredentials{username: "Noam",
-                                 password: "Chomsky"}
-    err2 := UpdateUser(db, creds, new_creds)
-    check(err2)
-    res4 := VerifyCredentials(db, new_creds)
-    if res4 != true {
-        t.Fail()
-    }
+	err3 := UpdateUser(db, creds, creds2) // `creds` should be invalid
+	if err3 == nil {
+		t.Fail()
+	}
 
-    err3 := UpdateUser(db, creds, creds2)  // `creds` should be invalid
-    if err3 == nil {
-        t.Fail()
-    }
-
-    // Delete
-    err4 := DeleteUser(db, new_creds)
-    if err4 != nil {
-        t.Fail()
-    }
-    res5 := VerifyCredentials(db, new_creds)
-    if res5 == true {
-        t.Fail()
-    }
+	// Delete
+	err4 := DeleteUser(db, new_creds)
+	if err4 != nil {
+		t.Fail()
+	}
+	res5 := VerifyCredentials(db, new_creds)
+	if res5 == true {
+		t.Fail()
+	}
 }
